5da18da44db299386b520b5cd2adedba37603c96
Xytabich <xytabich@gmail.com>
2020-12-25 22:31:05 +0300

    buffer writer/reader

---
+++ UNet/ByteBufferReader.cs
@@ -0,0 +1,402 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+
+namespace UNet
+{
+	/// <summary>
+	/// Reads data structures from byte array
+	/// BE byte order.
+	/// </summary>
+	public class ByteBufferReader : UdonSharpBehaviour
+	{
+		private const int BIT8 = 8;
+		private const int BIT16 = 16;
+		private const int BIT24 = 24;
+		private const int BIT32 = 32;
+		private const int BIT40 = 40;
+		private const int BIT48 = 48;
+		private const int BIT56 = 56;
+
+		private const uint FLOAT_SIGN_BIT = 0x80000000;
+		private const uint FLOAT_EXP_MASK = 0x7F800000;
+		private const uint FLOAT_COEF_MASK = 0x007FFFFF;
+
+		private int[] decimalBuffer = new int[4];
+		private byte[] guidBuffer = new byte[16];
+
+		#region common types
+		public bool ReadBool(byte[] buffer, int index)
+		{
+			return buffer[index] == 1;
+		}
+
+		public char ReadChar(byte[] buffer, int index)
+		{
+			return (char)ReadInt16(buffer, index);
+		}
+
+		public sbyte ReadSByte(byte[] buffer, int index)
+		{
+			int value = buffer[index];
+			if(value > 0x80) value = value - 0xFF;
+			return Convert.ToSByte(value);
+		}
+
+		public short ReadInt16(byte[] buffer, int index)
+		{
+			int value = buffer[index] << BIT8 | buffer[index + 1];
+			if(value > 0x8000) value = value - 0xFFFF;
+			return Convert.ToInt16(value);
+		}
+
+		public ushort ReadUInt16(byte[] buffer, int index)
+		{
+			return Convert.ToUInt16(buffer[index] << BIT8 | buffer[index + 1]);
+		}
+
+		public int ReadInt32(byte[] buffer, int index)
+		{
+			int value = 0;
+			value |= (int)buffer[index] << BIT24;
+			index++;
+			value |= (int)buffer[index] << BIT16;
+			index++;
+			value |= (int)buffer[index] << BIT8;
+			index++;
+			value |= (int)buffer[index];
+			return value;
+		}
+
+		public uint ReadUInt32(byte[] buffer, int index)
+		{
+			uint value = 0;
+			value |= (uint)buffer[index] << BIT24;
+			index++;
+			value |= (uint)buffer[index] << BIT16;
+			index++;
+			value |= (uint)buffer[index] << BIT8;
+			index++;
+			value |= (uint)buffer[index];
+			return value;
+		}
+
+		public long ReadInt64(byte[] buffer, int index)
+		{
+			long value = 0;
+			value |= (long)buffer[index] << BIT56;
+			index++;
+			value |= (long)buffer[index] << BIT48;
+			index++;
+			value |= (long)buffer[index] << BIT40;
+			index++;
+			value |= (long)buffer[index] << BIT32;
+			index++;
+			value |= (long)buffer[index] << BIT24;
+			index++;
+			value |= (long)buffer[index] << BIT16;
+			index++;
+			value |= (long)buffer[index] << BIT8;
+			index++;
+			value |= (long)buffer[index];
+			return value;
+		}
+
+		public ulong ReadUInt64(byte[] buffer, int index)
+		{
+			ulong value = 0;
+			value |= (ulong)buffer[index] << BIT56;
+			index++;
+			value |= (ulong)buffer[index] << BIT48;
+			index++;
+			value |= (ulong)buffer[index] << BIT40;
+			index++;
+			value |= (ulong)buffer[index] << BIT32;
+			index++;
+			value |= (ulong)buffer[index] << BIT24;
+			index++;
+			value |= (ulong)buffer[index] << BIT16;
+			index++;
+			value |= (ulong)buffer[index] << BIT8;
+			index++;
+			value |= (ulong)buffer[index];
+			return value;
+		}
+
+		public float ReadSingle(byte[] buffer, int index)
+		{
+			uint value = ReadUInt32(buffer, index);
+			if(value == 0 || value == FLOAT_SIGN_BIT) return 0f;
+
+			if((value & FLOAT_EXP_MASK) == FLOAT_EXP_MASK)
+			{
+				if((value & FLOAT_COEF_MASK) == FLOAT_COEF_MASK) return float.NaN;
+				return (value & FLOAT_SIGN_BIT) == FLOAT_SIGN_BIT ? float.NegativeInfinity : float.PositiveInfinity;
+			}
+
+			int exp = (int)((value & FLOAT_EXP_MASK) >> 23);
+			float coeff = (float)(value & FLOAT_COEF_MASK) / (2 << (21 + (exp > 0 ? 1 : 0)));
+			if(exp > 0) coeff += 1f;
+			float result = coeff * Mathf.Pow(2, exp - 127);
+			if((value & FLOAT_SIGN_BIT) == FLOAT_SIGN_BIT) result = -result;
+			return result;
+		}
+		#endregion
+
+		#region special types
+		public float ReadHalf(byte[] buffer, int index)
+		{
+			return Mathf.HalfToFloat(ReadUInt16(buffer, index));
+		}
+
+		public DateTime ReadDateTime(byte[] buffer, int index)
+		{
+			return DateTime.FromBinary(ReadInt64(buffer, index));
+		}
+
+		public TimeSpan ReadTimeSpan(byte[] buffer, int index)
+		{
+			return new TimeSpan(ReadInt64(buffer, index));
+		}
+
+		public decimal ReadDecimal(byte[] buffer, int index)
+		{
+			decimalBuffer[0] = ReadInt32(buffer, index);
+			index += 4;
+			decimalBuffer[1] = ReadInt32(buffer, index);
+			index += 4;
+			decimalBuffer[2] = ReadInt32(buffer, index);
+			index += 4;
+			decimalBuffer[3] = ReadInt32(buffer, index);
+			return new decimal(decimalBuffer);
+		}
+
+		public Guid ReadGuid(byte[] buffer, int index)
+		{
+			for(var i = 0; i < 16; i++)
+			{
+				guidBuffer[i] = buffer[index + i];
+			}
+			return new Guid(guidBuffer);
+		}
+
+		/// <summary>
+		/// Reads a variable-length unsigned integer.
+		/// </summary>
+		/// <remarks>To get size of integer use <see cref="GetVarUInt32Size"/></remarks>
+		public uint ReadVarUInt32(byte[] buffer, int index)
+		{
+			uint value = 0;
+			uint part;
+			int i = 0;
+			do
+			{
+				value <<= 7;
+				part = buffer[index];
+				value |= part & 0x7F;
+
+				i++;
+				if(i > 4)
+				{
+					Debug.LogError("Variable uint has invalid format");
+					return 0;
+				}
+			}
+			while((part & 0x80) != 0);
+
+			return value;
+		}
+
+		/// <summary>
+		/// Returns the number of bytes used for a variable-length unsigned integer.
+		/// </summary>
+		public int GetVarUInt32Size(byte[] buffer, int index)
+		{
+			int i = 0;
+			while((buffer[index + i] & 0x80) != 0)
+			{
+				i++;
+				if(i > 4)
+				{
+					Debug.LogError("Variable uint has invalid format");
+					return 0;
+				}
+			}
+			return i + 1;
+		}
+		#endregion
+
+		#region unity types
+		public Vector2 ReadVector2(byte[] buffer, int index)
+		{
+			float x = ReadSingle(buffer, index);
+			index += 4;
+			float y = ReadSingle(buffer, index);
+
+			return new Vector2(x, y);
+		}
+
+		public Vector3 ReadVector3(byte[] buffer, int index)
+		{
+			float x = ReadSingle(buffer, index);
+			index += 4;
+			float y = ReadSingle(buffer, index);
+			index += 4;
+			float z = ReadSingle(buffer, index);
+
+			return new Vector3(x, y, z);
+		}
+
+		public Vector4 ReadVector4(byte[] buffer, int index)
+		{
+			float x = ReadSingle(buffer, index);
+			index += 4;
+			float y = ReadSingle(buffer, index);
+			index += 4;
+			float z = ReadSingle(buffer, index);
+			index += 4;
+			float w = ReadSingle(buffer, index);
+
+			return new Vector4(x, y, z, w);
+		}
+
+		public Quaternion ReadQuaternion(byte[] buffer, int index)
+		{
+			float x = ReadSingle(buffer, index);
+			index += 4;
+			float y = ReadSingle(buffer, index);
+			index += 4;
+			float z = ReadSingle(buffer, index);
+			index += 4;
+			float w = ReadSingle(buffer, index);
+
+			return new Quaternion(x, y, z, w);
+		}
+		public Vector2 ReadHalfVector2(byte[] buffer, int index)
+		{
+			float x = ReadHalf(buffer, index);
+			index += 2;
+			float y = ReadHalf(buffer, index);
+
+			return new Vector2(x, y);
+		}
+
+		public Vector3 ReadHalfVector3(byte[] buffer, int index)
+		{
+			float x = ReadHalf(buffer, index);
+			index += 2;
+			float y = ReadHalf(buffer, index);
+			index += 2;
+			float z = ReadHalf(buffer, index);
+
+			return new Vector3(x, y, z);
+		}
+
+		public Vector4 ReadHalfVector4(byte[] buffer, int index)
+		{
+			float x = ReadHalf(buffer, index);
+			index += 2;
+			float y = ReadHalf(buffer, index);
+			index += 2;
+			float z = ReadHalf(buffer, index);
+			index += 2;
+			float w = ReadHalf(buffer, index);
+
+			return new Vector4(x, y, z, w);
+		}
+
+		public Quaternion ReadHalfQuaternion(byte[] buffer, int index)
+		{
+			float x = ReadHalf(buffer, index);
+			index += 2;
+			float y = ReadHalf(buffer, index);
+			index += 2;
+			float z = ReadHalf(buffer, index);
+			index += 2;
+			float w = ReadHalf(buffer, index);
+
+			return new Quaternion(x, y, z, w);
+		}
+		#endregion
+
+		#region strings
+		/// <summary>
+		/// Reads ascii string with <see cref="ReadVarUInt32"/> length prefix
+		/// </summary>
+		public string ReadVarASCIIString(byte[] buffer, int index)
+		{
+			uint length = ReadVarUInt32(buffer, index);
+			index += GetVarUInt32Size(buffer, index);
+			return ReadASCIIString((int)length, buffer, index);
+		}
+
+		/// <summary>
+		/// Reads the specified number of bytes and converts it to an ascii string
+		/// </summary>
+		public string ReadASCIIString(int bytesCount, byte[] buffer, int index)
+		{
+			char[] chars = new char[bytesCount];
+			for(var i = 0; i < bytesCount; i++)
+			{
+				chars[i] = Convert.ToChar(buffer[index + i] & 0x7F);
+			}
+			return new string(chars);
+		}
+
+		/// <summary>
+		/// Reads utf-8 string with <see cref="ReadVarUInt32"/> length prefix
+		/// </summary>
+		public string ReadVarUTF8String(byte[] buffer, int index)
+		{
+			uint length = ReadVarUInt32(buffer, index);
+			index += GetVarUInt32Size(buffer, index);
+			return ReadUTF8String((int)length, buffer, index);
+		}
+
+		/// <summary>
+		/// Reads the specified number of bytes and converts it to an utf-8 string
+		/// </summary>
+		public string ReadUTF8String(int bytesCount, byte[] buffer, int index)
+		{
+			string str = "";
+			int character = 0;
+			int charCounter = 0;
+			for(var i = 0; i < bytesCount; i++)
+			{
+				int value = buffer[index];
+				if((value & 0x80) == 0)
+				{
+					str += (char)value;
+					charCounter = 0;
+				}
+				else if((value & 0xC0) == 0x80)
+				{
+					if(charCounter > 0)
+					{
+						character = character << 6 | (value & 0x3F);
+						charCounter--;
+						if(charCounter == 0) str += char.ConvertFromUtf32(character);
+					}
+				}
+				else if((value & 0xE0) == 0xC0)
+				{
+					charCounter = 1;
+					character = value & 0x1F;
+				}
+				else if((value & 0xF0) == 0xE0)
+				{
+					charCounter = 2;
+					character = value & 0x0F;
+				}
+				else if((value & 0xF8) == 0xF0)
+				{
+					charCounter = 3;
+					character = value & 0x07;
+				}
+				index++;
+			}
+			return str;
+		}
+		#endregion
+	}
+}
---
+++ UNet/ByteBufferWriter.cs
@@ -0,0 +1,400 @@
+﻿using System;
+using UdonSharp;
+using UnityEngine;
+
+namespace UNet
+{
+	/// <summary>
+	/// Writes data structures to byte array.
+	/// BE byte order.
+	/// </summary>
+	public class ByteBufferWriter : UdonSharpBehaviour
+	{
+		private const int BIT8 = 8;
+		private const int BIT16 = 16;
+		private const int BIT24 = 24;
+		private const int BIT32 = 32;
+		private const int BIT40 = 40;
+		private const int BIT48 = 48;
+		private const int BIT56 = 56;
+
+		private const uint FLOAT_SIGN_BIT = 0x80000000;
+		private const uint FLOAT_EXP_MASK = 0x7F800000;
+		private const uint FLOAT_COEF_MASK = 0x007FFFFF;
+
+		#region common types
+		public int WriteBool(bool value, byte[] buffer, int index)
+		{
+			if(value) buffer[index] = 1;
+			else buffer[index] = 0;
+			return 1;
+		}
+
+		public int WriteChar(char value, byte[] buffer, int index)
+		{
+			return WriteInt16((short)value, buffer, index);
+		}
+
+		public int WriteSByte(sbyte value, byte[] buffer, int index)
+		{
+			buffer[index] = (byte)(value < 0 ? (value + 0xFF) : value);
+			return 1;
+		}
+
+		public int WriteInt16(short value, byte[] buffer, int index)
+		{
+			int tmp = value < 0 ? (value + 0xFFFF) : value;
+			buffer[index] = (byte)(tmp >> BIT8);
+			index++;
+			buffer[index] = (byte)(tmp & 0xFF);
+			return 2;
+		}
+
+		public int WriteUInt16(ushort value, byte[] buffer, int index)
+		{
+			int tmp = Convert.ToInt32(value);
+			buffer[index] = (byte)(tmp >> BIT8);
+			index++;
+			buffer[index] = (byte)(tmp & 0xFF);
+			return 2;
+		}
+
+		public int WriteInt32(int value, byte[] buffer, int index)
+		{
+			buffer[index] = (byte)((value >> BIT24) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT16) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT8) & 0xFF);
+			index++;
+			buffer[index] = (byte)(value & 0xFF);
+			return 4;
+		}
+
+		public int WriteUInt32(uint value, byte[] buffer, int index)
+		{
+			buffer[index] = (byte)((value >> BIT24) & 255u);
+			index++;
+			buffer[index] = (byte)((value >> BIT16) & 255u);
+			index++;
+			buffer[index] = (byte)((value >> BIT8) & 255u);
+			index++;
+			buffer[index] = (byte)(value & 255u);
+			return 4;
+		}
+
+		public int WriteInt64(long value, byte[] buffer, int index)
+		{
+			buffer[index] = (byte)((value >> BIT56) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT48) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT40) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT32) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT24) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT16) & 0xFF);
+			index++;
+			buffer[index] = (byte)((value >> BIT8) & 0xFF);
+			index++;
+			buffer[index] = (byte)(value & 0xFF);
+			return 8;
+		}
+
+		public int WriteUInt64(ulong value, byte[] buffer, int index)
+		{
+			buffer[index] = (byte)((value >> BIT56) & 255ul);
+			index++;
+			buffer[index] = (byte)((value >> BIT48) & 255ul);
+			index++;
+			buffer[index] = (byte)((value >> BIT40) & 255ul);
+			index++;
+			buffer[index] = (byte)((value >> BIT32) & 255ul);
+			index++;
+			buffer[index] = (byte)((value >> BIT24) & 255ul);
+			index++;
+			buffer[index] = (byte)((value >> BIT16) & 255ul);
+			index++;
+			buffer[index] = (byte)((value >> BIT8) & 255ul);
+			index++;
+			buffer[index] = (byte)(value & 255ul);
+			return 8;
+		}
+
+		public int WriteSingle(float value, byte[] buffer, int index)
+		{
+			uint tmp = 0;
+			if(float.IsNaN(value))
+			{
+				tmp = FLOAT_EXP_MASK | FLOAT_COEF_MASK;
+			}
+			else if(float.IsInfinity(value))
+			{
+				tmp = FLOAT_EXP_MASK;
+				if(float.IsNegativeInfinity(value)) tmp |= FLOAT_SIGN_BIT;
+			}
+			else if(value != 0f)
+			{
+				if(value < 0f)
+				{
+					value = -value;
+					tmp |= FLOAT_SIGN_BIT;
+				}
+
+				int exp = 127;
+				while(value > 2f)
+				{
+					value *= 0.5f;
+					exp++;
+				}
+				while(value < 1f)
+				{
+					value *= 2f;
+					exp--;
+					if(exp <= 0) break;
+				}
+
+				tmp |= Convert.ToUInt32(exp << 23) & FLOAT_EXP_MASK;
+				tmp |= Convert.ToUInt32(value * (2 << (21 + (exp > 0 ? 1 : 0)))) & FLOAT_COEF_MASK;
+			}
+			return WriteUInt32(tmp, buffer, index);
+		}
+		#endregion
+
+		#region special types
+		public int WriteHalf(float value, byte[] buffer, int index)
+		{
+			return WriteUInt16(Mathf.FloatToHalf(value), buffer, index);
+		}
+
+		public int WriteDateTime(DateTime value, byte[] buffer, int index)
+		{
+			return WriteInt64(value.ToBinary(), buffer, index);
+		}
+
+		public int WriteTimeSpan(TimeSpan value, byte[] buffer, int index)
+		{
+			return WriteInt64(value.Ticks, buffer, index);
+		}
+
+		public int WriteDecimal(decimal value, byte[] buffer, int index)
+		{
+			var tmp = Decimal.GetBits(value);
+			WriteInt32(tmp[0], buffer, index);
+			index += 4;
+			WriteInt32(tmp[1], buffer, index);
+			index += 4;
+			WriteInt32(tmp[2], buffer, index);
+			index += 4;
+			WriteInt32(tmp[3], buffer, index);
+			return 16;
+		}
+
+		public int WriteGuid(Guid value, byte[] buffer, int index)
+		{
+			var tmp = value.ToByteArray();
+			tmp.CopyTo(buffer, index);
+			return 16;
+		}
+
+		public int WriteVarUInt32(uint value, byte[] buffer, int index)
+		{
+			int size = 1;
+			while(value > 127)
+			{
+				buffer[index] = (byte)(value & 0x7F);
+				value >>= 7;
+				size++;
+			}
+			buffer[index] = (byte)value;
+			return size;
+		}
+		#endregion
+
+		#region unity types
+		public int WriteVector2(Vector2 value, byte[] buffer, int index)
+		{
+			WriteSingle(value.x, buffer, index);
+			index += 4;
+			WriteSingle(value.y, buffer, index);
+			return 8;
+		}
+
+		public int WriteVector3(Vector3 value, byte[] buffer, int index)
+		{
+			WriteSingle(value.x, buffer, index);
+			index += 4;
+			WriteSingle(value.y, buffer, index);
+			index += 4;
+			WriteSingle(value.z, buffer, index);
+			return 12;
+		}
+
+		public int WriteVector4(Vector4 value, byte[] buffer, int index)
+		{
+			WriteSingle(value.x, buffer, index);
+			index += 4;
+			WriteSingle(value.y, buffer, index);
+			index += 4;
+			WriteSingle(value.z, buffer, index);
+			index += 4;
+			WriteSingle(value.w, buffer, index);
+			return 16;
+		}
+
+		public int WriteQuaternion(Quaternion value, byte[] buffer, int index)
+		{
+			WriteSingle(value.x, buffer, index);
+			index += 4;
+			WriteSingle(value.y, buffer, index);
+			index += 4;
+			WriteSingle(value.z, buffer, index);
+			index += 4;
+			WriteSingle(value.w, buffer, index);
+			return 16;
+		}
+
+		public int WriteHalfVector2(Vector2 value, byte[] buffer, int index)
+		{
+			WriteHalf(value.x, buffer, index);
+			index += 2;
+			WriteHalf(value.y, buffer, index);
+			return 4;
+		}
+
+		public int WriteHalfVector3(Vector3 value, byte[] buffer, int index)
+		{
+			WriteHalf(value.x, buffer, index);
+			index += 2;
+			WriteHalf(value.y, buffer, index);
+			index += 2;
+			WriteHalf(value.z, buffer, index);
+			return 6;
+		}
+
+		public int WriteHalfVector4(Vector4 value, byte[] buffer, int index)
+		{
+			WriteHalf(value.x, buffer, index);
+			index += 2;
+			WriteHalf(value.y, buffer, index);
+			index += 2;
+			WriteHalf(value.z, buffer, index);
+			index += 2;
+			WriteHalf(value.w, buffer, index);
+			return 8;
+		}
+
+		public int WriteHalfQuaternion(Quaternion value, byte[] buffer, int index)
+		{
+			WriteHalf(value.x, buffer, index);
+			index += 2;
+			WriteHalf(value.y, buffer, index);
+			index += 2;
+			WriteHalf(value.z, buffer, index);
+			index += 2;
+			WriteHalf(value.w, buffer, index);
+			return 8;
+		}
+		#endregion
+
+		#region strings
+		public int WriteASCIIString(string str, byte[] buffer, int index)
+		{
+			int len = str.Length;
+			for(var i = 0; i < len; i++)
+			{
+				buffer[index + i] = (byte)(str[i] & 0x7F);
+			}
+			return len;
+		}
+
+		public int WriteVarASCIIString(string str, byte[] buffer, int index)
+		{
+			int size = WriteVarUInt32((uint)str.Length, buffer, index);
+			index += size;
+			size += WriteASCIIString(str, buffer, index);
+			return size;
+		}
+
+		public int WriteUTF8String(string str, byte[] buffer, int index)
+		{
+			int byteIndex = 0;
+			int len = str.Length;
+			for(var i = 0; i < len; i++)
+			{
+				int value = char.ConvertToUtf32(str, i);
+				if(value < 0x80)
+				{
+					buffer[index + byteIndex] = (byte)value;
+				}
+				else if(value < 0x0800)
+				{
+					buffer[index + byteIndex] = (byte)(value >> 6 | 0xC0);
+					byteIndex++;
+					buffer[index + byteIndex] = (byte)(value & 0x3F | 0x80);
+				}
+				else if(value < 0x010000)
+				{
+					buffer[index + byteIndex] = (byte)(value >> 12 | 0xE0);
+					byteIndex++;
+					buffer[index + byteIndex] = (byte)((value >> 6) & 0x3F | 0x80);
+					byteIndex++;
+					buffer[index + byteIndex] = (byte)(value & 0x3F | 0x80);
+				}
+				else
+				{
+					buffer[index + byteIndex] = (byte)(value >> 18 | 0xF0);
+					byteIndex++;
+					buffer[index + byteIndex] = (byte)((value >> 12) & 0x3F | 0x80);
+					byteIndex++;
+					buffer[index + byteIndex] = (byte)((value >> 6) & 0x3F | 0x80);
+					byteIndex++;
+					buffer[index + byteIndex] = (byte)(value & 0x3F | 0x80);
+				}
+				byteIndex++;
+				if(char.IsSurrogate(str, i)) i++;
+			}
+			return byteIndex;
+		}
+
+		public int WriteVarUTF8String(string str, byte[] buffer, int index)
+		{
+			int size = GetUTF8StringSize(str);
+			size = WriteVarUInt32((uint)size, buffer, index);
+			index += size;
+			size += WriteUTF8String(str, buffer, index);
+			return size;
+		}
+
+		public int GetUTF8StringSize(string str)
+		{
+			int byteIndex = 0;
+			int len = str.Length;
+			for(var i = 0; i < len; i++)
+			{
+				int value = char.ConvertToUtf32(str, i);
+				if(value >= 0x80)
+				{
+					if(value < 0x0800)
+					{
+						byteIndex++;
+					}
+					else if(value < 0x010000)
+					{
+						byteIndex += 2;
+					}
+					else
+					{
+						byteIndex += 3;
+					}
+				}
+				byteIndex++;
+				if(char.IsSurrogate(str, i)) i++;
+			}
+			return byteIndex;
+		}
+		#endregion
+	}
+}
